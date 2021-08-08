84d737dca88b7f5737dc177af90e08d8e279c474
Bartłomiej Guzek <BartekGuzek@gmail.com>
2021-08-08 19:04:05 +0200

    Add project files.

---
+++ code/Game.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+/// <summary>
+/// This is the heart of the gamemode. It's responsible
+/// for creating the player and stuff.
+/// </summary>
+[Library( "Strife", Title = "Strife" )]
+partial class StrifeGame : Game
+{
+	public StrifeGame()
+	{
+		//
+		// Create the HUD entity. This is always broadcast to all clients
+		// and will create the UI panels clientside. It's accessible 
+		// globally via Hud.Current, so we don't need to store it.
+		//
+		if ( IsServer )
+		{
+			new PlayerHud();
+		}
+	}
+
+	public override void PostLevelLoaded()
+	{
+		base.PostLevelLoaded();
+
+		ItemRespawn.Init();
+	}
+
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
+
+		var player = new StrifePlayer();
+		player.Respawn();
+
+		cl.Pawn = player;
+	}
+}
